15a4874d3870666ac187e8e821dd298b83c8151a
thangnguyen-imac <nguyenhongthangnl@gmail.com>
2016-07-28 06:37:11 +0700

    modified configure

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,8 @@
-/*var express = require('express'),
-    router = express.Router(),
-    index = require('../controllers/index');*/
-    
-module.exports = function(app){
-  //app.get('/', index.home);
-  app.get('/', function(req, res){
-    res.render('index', {title: 'Running export function'});
-  });
-  
-  //app.use(router);
+var express = require('express'),
+       router = express.Router(),
+       home = require('../controllers/index');
+
+   module.exports = function(app) {
+       router.get('/', home.index);
+       app.use(router);
 };
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var express = require('express'); // include express framework
-
-var app = express(),
-config = require('./config/configure'); // assigned the express framework to app variable.
-
+var express = require('express'), // include express framework
+config = require('./config/configure'), // assigned the express framework to app variable.
+app = express();
 
 app.set('port', process.env.PORT || 3000); // get enviroment port and assign to port variable
 app.set('address', process.env.IP || 'localhost'); // get enviroment ip assign to address variable
 
 // set folder views for this app
 app.set('views', __dirname + '/views');
-app = config(app);
+
 console.log("To here");
 var port = app.get('port');
+app = config(app);
 // Start webserver
 app.listen(port, app.get('address'), function(){
    console.log("Server is running on port ");
